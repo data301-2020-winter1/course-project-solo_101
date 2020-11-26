737ceb4cc617fce946cf1d32b6e2edab7d0695b3
veengren <46636094+veengren@users.noreply.github.com>
2020-11-26 14:42:27 -0700

    Made recommended changes

--- analysis/Garrett/scripts/project_functions.py
+++ analysis/Garrett/scripts/project_functions.py
@@ -3,16 +3,19 @@ import numpy as np
 import seaborn as sns
 
 def Procces(path):
+    #Renames a few columns and drops na valuesin tongue activity nad mean elevation
     data_chain_1 = (p.read_csv(path,encoding = 'ANSI')
      .rename(columns={'wgi_glacier_id':'Glacier ID','political_unit':'Country'})
      .dropna(subset = ['tongue_activity','mean_elev'])
      .reset_index()
     )
+    #removes unwanted columns
     data_chain_2 = (data_chain_1
                    .loc[:,['tongue_activity','Glacier ID','Country','continent_code','lat','lon','orientation_acc','orientation_abl','source_nourish','mean_elev']]
                    .reset_index()
                    .drop(columns=['index'])
                    )
+    #replaces country code with country name
     dat = data_chain_2.replace(['AF','AQ','AR','AT','BO','BT','CA','CH','CL','CN','CO','DE','EC','ES','FR','GL','GS','HM','ID','IN','IS','IT','KE','MX','NO','NP','NZ','PE','PK','SE','SU',
                               'TF','TZ','UG',
               'US','VE','ZA','ZR'],['Afghanistan','Antarctica','Argentina','Austria','Bolivia','Bhutan','Canada','Switzerland','Chile','China','Colombia','Germany','Ecuador','Spain',
